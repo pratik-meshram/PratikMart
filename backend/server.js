81aa08cc294f60bd0b44a0b6d5749b001be7739b
Pratik Meshram <pratikmeshram0021@gmail.com>
2026-04-25 16:49:21 +0530

    final code

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,14 @@ app.use('/api/orders', require('./routes/orders'));
 app.use('/api/admin', require('./routes/admin'));
 app.use('/api/payment', require('./routes/payment'));
 
-mongoose.connect(process.env.MONGO_URI)
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => {
     console.log('MongoDB connected');
+
     app.listen(process.env.PORT, () =>
       console.log(`Server running on port ${process.env.PORT}`)
     );
